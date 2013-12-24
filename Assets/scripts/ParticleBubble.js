d7ed32a1935c49b40a0efd86e7e7ca5ed19b834c
Yukai <yukaihuang1993@hotmail.com>
2013-12-24 23:20:42 +0800

    like the first time > <

--- Assets/scripts/ParticleBubble.js
+++ Assets/scripts/ParticleBubble.js
@@ -1,9 +1,17 @@
 //var circleMaterial : Material[];
 var Rate : float;
+var MainCamera : Camera;
+var Tespos : Vector3;
+var inputRegion : float;
+var QueueFunctions : TheQueue;
 
+MainCamera = GameObject.Find("Camera").GetComponent("Camera");
+QueueFunctions = GameObject.Find("Camera").GetComponent(TheQueue);
 
 var _particles : ParticleSystem.Particle[];
 _particles = new ParticleSystem.Particle[particleSystem.maxParticles];
+
+//var touched : boolean[particleSystem.maxParticles];
 
 //set particle[i].lifetime to zero to destroy the particle
 
@@ -14,12 +22,81 @@ function Start (){
 function Update (){
 
 	//set particles in _particles array
-	particleSystem.GetParticles(_particles);
-	for (var each in _particles)
-		if (each.position.magnitude)
-			print(each.position);
+	var amount = particleSystem.GetParticles(_particles);
 	
+	//must use this for method to pass the correct item
+	//not "for (var each in _particle)"
+	//still don't know why :p
+	for (var i = 0; i < amount; i++)
+	{	
+		if(isTouched(_particles[i]))
+		{
+			var index = QueueFunctions.WhichBubble(gameObject.name);
+			
+			if (QueueFunctions.DestroyCircle(index))
+			{
+				//destroy single particle
+				_particles[i].lifetime = 0;
+			}
+			
+		}
+
+	}
+	//set back the changes to original particle array
+	particleSystem.SetParticles(_particles, amount);
+	
+	//set the emission rate
 	GetComponent(ParticleSystem).emissionRate = Rate;
 
 	
+}
+
+//position range from (x, y) = (0, 0.89) to (0.99, 0.89)
+//pixel form (240.0, 503.0, 22.7) to (324.5, 503.0, 22.7)
+//later stage animation
+function FlyAway(theParticle : ParticleSystem.Particle) 
+{
+
+}
+
+
+function isTouched(theParticle : ParticleSystem.Particle) : boolean 
+{
+
+	var counts = Input.touchCount;
+	var particlePos = Vector2.zero;
+	var tempVector : Vector3 = MainCamera.WorldToScreenPoint(theParticle.position);
+
+	particlePos.x = tempVector.x;
+	particlePos.y = tempVector.y;
+
+	for (var i = 0; i < counts; i++)
+	{
+		var touch : Touch = Input.GetTouch(i);
+
+		var between : Vector2 = touch.position - particlePos;
+
+		if (between.magnitude <= inputRegion) return true;
+			else return false;
+	}
+
+	
+	//---------------------------------------------//
+	//add mouse support for testing
+	var click2 : Vector3 = Input.mousePosition;
+	var click : Vector2;
+
+	click.x = click2.x;
+	click.y = click2.y;
+
+	//print(click);
+
+	var between2 : Vector2 = click - particlePos;
+
+	if ( between2.magnitude <= inputRegion)
+		return true;
+	else 
+		return false;
+	//----------------end test block------------//
+
 }
---
+++ Assets/scripts/TheQueue.js
@@ -0,0 +1,135 @@
+﻿#pragma strict
+
+//Set GameObject array size in Inspector window
+var circles : GameObject[];
+var circlesPrefab : GameObject[];
+
+//Random color of balls - the order list
+var orders : int[];
+var tarPosX : float[];
+
+
+//todo: some GUI scaling
+//write here.........
+//var nowInset = new Rect(400.0, 775.0, 60.0, 60.0);
+var nowPos = Vector3(0.8, 0.90, 0);
+
+//Left-border: inset.x = 8
+
+var pointedCircle = 0;
+
+
+function Start () {
+	GenerateQueue();
+}
+
+function Update () {
+ 	MoveToLeftMost();
+ 	if (pointedCircle == circles.length)
+ 	{
+ 		pointedCircle = 0;
+ 		nowPos = Vector3(0.8, 0.90, 0);
+ 		GenerateQueue();
+ 		//Waits(5);
+ 	}
+ 		
+
+}
+
+function GenerateQueue() {
+
+	var i = 0;
+	//create a list of random circle order
+	for (var number in orders) 
+	{
+		number = Random.Range(0, 8);
+		
+		circles[i] = Instantiate(circlesPrefab[number], Vector3(0, 0, 0), circlesPrefab[number].transform.rotation);
+
+		//Set Pixel Inset
+		//circles[i].guiTexture.pixelInset = nowInset;
+		circles[i].transform.position = nowPos;
+
+		//Shifted Right for every circle
+		//nowInset.x += nowInset.width * 6.0 / 5.0;
+		nowPos.x += 0.15;
+
+		//count for each circle
+		i++;
+	}
+
+	//set each move Target position
+	var tarX = 0.02;
+	for (var each in tarPosX)
+	{
+		each = tarX;
+		tarX += 0.15;
+	}
+}
+
+//move all circles to left
+function MoveToLeftMost(){
+	//var movePixel = 8 - circles[pointedCircle].guiTexture.pixelInset.x;
+
+	var amount = circles.length;
+	for (var i = pointedCircle; i < amount; i++)
+	{
+		circles[i].transform.position = Vector3.MoveTowards(circles[i].transform.position, Vector3(tarPosX[i], 0.91, 0), Time.deltaTime);
+	}
+}
+
+function DestroyCircle(index : int) : boolean
+{
+	var Namae = circlesPrefab[index].name + "(Clone)";
+	
+	//Debug.Log("pointed circle name = \"" + circles[pointedCircle].name + "\", prefab index name = \"" + Namae + "\"");
+
+	if (circles[pointedCircle].name == Namae)
+	{
+		for (var each in tarPosX)
+		{
+			each -= 0.15;
+		}
+		Destroy(circles[pointedCircle]);
+
+		pointedCircle++;
+		return true;
+	}
+	else return false;
+}
+
+function WhichBubble(bubbleName : String) : int
+{
+	switch(bubbleName)
+	{
+		case "bubble1":
+			return 0;
+			break;
+		case "bubble2":
+			return 1;
+			break;
+		case "bubble3":
+			return 2;
+			break;
+		case "bubble4":
+			return 3;
+			break;
+		case "bubble5":
+			return 4;
+			break;
+		case "bubble6":
+			return 5;
+			break;
+		case "bubble7":
+			return 6;
+			break;
+		case "bubble8":
+			return 7;
+			break;
+	}
+}
+
+function Waits(seconds : float)
+{
+	yield WaitForSeconds(seconds);
+}
